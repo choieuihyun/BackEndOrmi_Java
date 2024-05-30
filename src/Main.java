812a3e017aabcf7ff65514fae23ae7b9c1b5c955
choieuihyun <amammmaa1@naver.com>
2024-05-30 09:34:34 +0900

    init

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    /**
+     * @author 굿
+     * @apiNote 이것은 무엇을 하는거냐?
+     */
+    public static void main(String[] args) {
+
+        int a = 10;
+        System.out.println("Hello world!");
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+
+
+    }
+
+    public void asdf() {
+
+
+
+    }
+}
+
+
